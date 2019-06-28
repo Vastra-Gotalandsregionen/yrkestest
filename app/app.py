50562e575ed79125b4fa7ef88b2b3b0cb70c53fc
Thomas Svenson <thomas@tsvenson.com>
2019-06-28 12:58:13 +0200

    Added start page and questions shown one after another.

--- app/app.py
+++ app/app.py
@@ -8,8 +8,13 @@ app.config['SECRET_KEY'] = 'secret!'
 
 @app.route("/")
 def yrkestest():
-    return render_template("fraga.html", questions=questions)
+    return render_template("start.html", questions=questions)
 
+
+@app.route("/fraga/<int:fraga_id>")
+def fraga(fraga_id):
+    content = [fraga_id + 1, questions[fraga_id]]
+    return render_template("fraga.html", content_data=content)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
